6e90029c45b8eee881ec517ca7b2d86b927ddbfc
DrarenThiralas <maxerdy@gmail.com>
2021-01-03 19:23:39 +0100

    Initial implementation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import prob_calculator
 from unittest import main
 
 hat = prob_calculator.Hat(blue=4, red=2, green=6)
+print()
+print(hat.contents)
 probability = prob_calculator.experiment(
     hat=hat,
     expected_balls={"blue": 2,
--- prob_calculator.py
+++ prob_calculator.py
@@ -4,5 +4,51 @@ import random
 
 class Hat:
 
+	def __init__(self, **kwargs):
+		self.contents = []
+		for item in kwargs.items():
+			color, amount = item
+			self.contents += [color for i in range(amount)]
+
+	def draw(self, amount):
+		toReturn = []
+		if amount >= len(self.contents):
+			toReturn += self.contents
+			self.contents = []
+		else:
+			for i in range(amount):
+				toDraw = random.randint(0, len(self.contents)-1)
+				toReturn += [self.contents[toDraw]]
+				self.contents = self.contents[:toDraw]+self.contents[toDraw+1:]
+
+		return toReturn
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+	random.seed()
+	successes = 0
+
+	for i in range(num_experiments):
+		tempHat = copy.deepcopy(hat)
+		drawnList = tempHat.draw(num_balls_drawn)
+		drawnDict = {}
+		for ball in drawnList:
+			if ball in drawnDict:
+				drawnDict[ball] += 1
+			else:
+				drawnDict[ball] = 1
+		experimentSuccess = True
+		for item in expected_balls.items():
+			ball, amount = item
+			if ball not in drawnDict:
+				experimentSuccess = False
+			else:
+				if drawnDict[ball] < amount:
+					experimentSuccess = False
+
+		if experimentSuccess:
+			successes += 1
+
+	return successes/num_experiments
+
+
